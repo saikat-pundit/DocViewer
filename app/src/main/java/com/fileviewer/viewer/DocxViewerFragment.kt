aecf06ce900e35a26af3fd1f9e37a722d7129ed1
saikat-pundit <saikatpundit@yandex.com>
2026-04-08 09:04:12 +0530

    Create DocxViewerFragment.kt

---
+++ app/src/main/java/com/fileviewer/viewer/DocxViewerFragment.kt
@@ -0,0 +1,49 @@
+package com.fileviewer.viewer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ScrollView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.fileviewer.parser.DocxParser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.FileInputStream
+
+class DocxViewerFragment : Fragment() {
+    
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val scrollView = ScrollView(requireContext())
+        val textView = TextView(requireContext())
+        textView.setPadding(50, 50, 50, 50)
+        textView.textSize = 16f
+        scrollView.addView(textView)
+        
+        val filePath = arguments?.getString("file_path")
+        filePath?.let {
+            loadDocx(it, textView)
+        }
+        
+        return scrollView
+    }
+    
+    private fun loadDocx(path: String, textView: TextView) {
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                val inputStream = FileInputStream(path)
+                val content = DocxParser.parse(requireContext(), inputStream)
+                withContext(Dispatchers.Main) {
+                    textView.text = content
+                }
+            }
+        }
+    }
+}
